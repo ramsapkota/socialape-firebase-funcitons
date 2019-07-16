2b077262c8ac584b2adaca16ed7c445c94d30f3c
ram.sapkota <ram.sapkota@sourcecode.com.np>
2019-07-16 09:23:37 +0545

    firebase cloud functions with little bit of express and firestorm databse

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyBTzdVKx6oXpDK_GGNAPQHqOwtR9IkhdBY",
+  authDomain: "socialape-79c85.firebaseapp.com",
+  databaseURL: "https://socialape-79c85.firebaseio.com",
+  projectId: "socialape-79c85",
+  storageBucket: "socialape-79c85.appspot.com",
+  messagingSenderId: "247051120256",
+  appId: "1:247051120256:web:f70f68f6ca459e6a"
+};
